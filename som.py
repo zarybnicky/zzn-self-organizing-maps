0a19ac9215c5be7a15dafa410c35f2ee1f7d37be
Jakub Zárybnický <kubaz@inuits.eu>
2019-11-10 00:55:50 +0100

    Try out an update step on mock data

--- som.py
+++ som.py
@@ -2,4 +2,35 @@
 
 import numpy as np
 
-print(np.empty((5, 2)))
+neighborhood = {
+    0: [1, 2],
+    1: [0, 3],
+    2: [0, 3],
+    3: [1, 2],
+}
+# weights = np.random((x, y), ?)
+weights = np.array([
+    (1, 2, 3),
+    (3, 2, 1),
+    (2, 3, 1),
+    (0, 0, 0),
+], dtype=float)
+vector = np.array((0, 1, 0))
+
+def theta(degrees_of_separation):
+    if degrees_of_separation == 0:
+        return 1.
+    else:
+        return 0.75
+
+def rate(epoch_number):
+    return 0.1
+
+# Best marching unit = idx of smallest ||weight - input||
+bmu = np.argmin(np.sum((weights - vector) ** 2, axis=1))
+neighbors = neighborhood[bmu]
+print(weights)
+weights[bmu] += theta(0) * rate(0) * (vector - weights[bmu])
+for n in neighbors:
+    weights[n] += theta(1) * rate(0) * (vector - weights[n])
+print(weights)
